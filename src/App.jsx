147271ee0fc4e7497a3de084d2ecd210dec86289
kahffi <daffaalkahffi24@gmail.com>
2024-07-11 22:43:30 +0700

    fix the import

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
-import { useAuthState } from "react-firebase-hooks";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 
 const firebaseConfig = {
