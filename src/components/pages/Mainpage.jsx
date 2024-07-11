c819b5891c0e06cb8ca121bf70fba721f4e8d455
kahffi <daffaalkahffi24@gmail.com>
2024-07-12 05:14:11 +0700

    added location track from leaflet

--- src/components/pages/Mainpage.jsx
+++ src/components/pages/Mainpage.jsx
@@ -1,12 +1,35 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+	MapContainer,
+	TileLayer,
+	Marker,
+	Popup,
+	useMapEvents,
+	useMap,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { useEffect, useState } from "react";
+let currentPosition;
+
+function FlyToPosition() {
+	const map = useMap();
+
+	// please change this line of code in the future
+	setTimeout(() => {
+		map.locate().on("locationfound", (e) => {
+			map.flyTo(e.latlng, 16);
+			currentPosition = e.latlng;
+		});
+		return null;
+	}, 3000);
+}
 
 const GMapView = () => {
 	const mapTilerApiKey = "KCwVKzPssuOk5YIeApg0"; // Ganti dengan API key Anda dari MapTiler
 
 	return (
 		<MapContainer
-			center={[51.505, -0.09]}
+			id="map"
+			center={[6.175, 106.8283]}
 			zoom={13}
 			style={{ height: "90vh", width: "100%" }}
 		>
@@ -14,6 +37,7 @@ const GMapView = () => {
 				url={`https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=${mapTilerApiKey}`}
 				attribution='&copy; <a href="https://www.maptiler.com/copyright/">MapTiler</a>'
 			/>
+			<FlyToPosition />
 		</MapContainer>
 	);
 };
