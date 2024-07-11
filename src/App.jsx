280ed9488eac7b3021e9d61c3875438fc1edc51f
kahffi <daffaalkahffi24@gmail.com>
2024-07-11 21:37:57 +0700

    adding firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+import { useAuthState } from "react-firebase-hooks";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyAEJMIWS-Q3En9G3xbqqk46pnBCs606Y7E",
+	authDomain: "gh-lima.firebaseapp.com",
+	projectId: "gh-lima",
+	storageBucket: "gh-lima.appspot.com",
+	messagingSenderId: "763942727917",
+	appId: "1:763942727917:web:96c087e42e62ccf8eb5ce4",
+	measurementId: "G-YJL27X0Z0X",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return <></>;
 }
 
-export default App
+export default App;
