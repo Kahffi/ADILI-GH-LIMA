35b9a8c8ab89ab49e55243191d82e94e36fd8492
kahffi <daffaalkahffi24@gmail.com>
2024-07-13 01:26:05 +0700

    adding form component

--- src/components/CaseForm.jsx
+++ src/components/CaseForm.jsx
@@ -1,3 +1,6 @@
+import "./CaseForm.css";
+import uploadImageIcon from "../assets/Upload-Image-Icon.png";
+
 class Case {
 	constructor(caseTitle, caseId, uploader, caseDescription, caseImages) {
 		this.caseTitle = caseTitle;
@@ -25,15 +28,51 @@ class Case {
 export const CaseForm = () => {
 	return (
 		<div className="case-form-container">
-			<form action="case-form">
+			<form className="case-form">
+				<h1>Buat Laporan</h1>
+
+				<div className="input-field upload-image">
+					<img
+						src={uploadImageIcon}
+						alt="Upload image icon"
+						style={{ width: "200px", height: "auto" }}
+					/>
+					<button type="button" id="upload-cover-btn">
+						Unggah Sampul
+					</button>
+					<input type="file" id="cover-image" style={{ display: "none" }} />
+				</div>
+
 				<div className="input-field">
-					<label htmlFor="case-title">Masukkan judul kasus</label>
+					<label htmlFor="case-title">Judul Laporan</label>
 					<input type="text" id="case-title" />
+				</div>
+
+				<div style={{ width: "100%" }}>
+					<p style={{ textAlign: "left" }}>Pilih satu kategori</p>
+					<div className="category-select-container">
+						<div className="top">
+							<button type="button" className="category-btn">
+								Salah Tagkap
+							</button>
+							<button type="button" className="category-btn">
+								Hukum Pidana
+							</button>
+						</div>
+						<div className="bottom">
+							<button type="button" className="category-btn">
+								Korupsi
+							</button>
+							<button type="button" className="category-btn">
+								No Viral No Justice
+							</button>
+						</div>
+					</div>
 				</div>
 
 				<div className="input-field">
 					<label htmlFor="case-description">Masukkan deskripsi kasus ini</label>
-					<input type="text" id="case-description" />
+					<textarea id="case-description" />
 				</div>
 
 				<div className="input-field">
@@ -43,7 +82,16 @@ export const CaseForm = () => {
 
 				<div className="input-field">
 					<label htmlFor="image-context">Berikan konteks untuk gambar</label>
-					<input type="file" id="image-context" />
+					<textarea id="image-context" />
+				</div>
+
+				<div className="button-container">
+					<button type="button" id="batalkan">
+						Batalkan
+					</button>
+					<button type="submit" id="submit">
+						Unggah
+					</button>
 				</div>
 			</form>
 		</div>
